53842d44d5094eede67ceee86ec88de85e5973fb
doop <75194878+codeuk@users.noreply.github.com>
2022-03-27 20:25:16 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+setup(
+  name="pipware",
+  version="1.01",
+  author="CodeUK",
+  author_email="d@doop.fun",
+  description="Malicious Python Module Experimentation",
+  long_description=open("README.md").read(),
+  long_description_content_type="text/markdown",
+  url="https://github.com/codeuk/pipware",
+  project_urls={
+    "GitHub": "https://github.com/codeuk/pipware",
+    "Bug Tracker": "https://github.com/codeuk/pipware/issues",
+  },
+  license="MIT",
+  keywords=["pipware","malware"],
+  classifiers = [
+  "Programming Language :: Python :: 3",
+  "License :: OSI Approved :: MIT License",
+  "Operating System :: Microsoft :: Windows",
+  "Development Status :: 5 - Production/Stable",
+  "Intended Audience :: Developers",
+  "Natural Language :: English",
+  "Topic :: Software Development"
+  ],
+  package_dir={"": "."},
+  packages=find_packages(where="."),
+  install_requires=['requests']
+)
